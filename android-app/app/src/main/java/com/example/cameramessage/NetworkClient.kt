5365c438676f62104dca77b89aeda895ad3a5848
simpson8687-ops <simpson8687@gmail.com>
2026-07-19 19:52:59 +0900

    config: update NetworkClient base url to local network IP for physical device connection

--- android-app/app/src/main/java/com/example/cameramessage/NetworkClient.kt
+++ android-app/app/src/main/java/com/example/cameramessage/NetworkClient.kt
@@ -5,7 +5,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 object NetworkClient {
     // Emulator loopback IP. Set this to your local server IP (e.g., http://192.168.x.x:8000/) for real devices.
-    private const val BASE_URL = "http://10.0.2.2:8000/"
+    private const val BASE_URL = "http://10.98.81.70:8000/"
 
     val api: ApiService by lazy {
         Retrofit.Builder()
